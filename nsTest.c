e1de39473bac116164a0d22cee8d1a0e23cbfee5
lm410 <1303106461@qq.com>
2018-06-26 23:32:10 +0800

    change

--- nsTest.c
+++ nsTest.c
@@ -5,11 +5,29 @@
 	> Created Time: 二  6/26 22:19:28 2018
  ************************************************************************/
 
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<stdlib.h>
 #include<stdio.h>
 
 int main() 
 {
-    printf("main--start a container");
-    
+    int childpid;  
+int i;  
+  
+if ((i = fork()) == 0){  
+    //child process  
+    char * execv_str[] = {"echo", "executed by execv",NULL};  
+    printf("child--%d", i);
+    if (execv("/usr/bin/echo",execv_str) <0 ){  
+        perror("error on exec");  
+        exit(0);  
+    }  
+}else{  
+    //parent process  
+    wait(&childpid);  
+    printf("execv done\n\n");  
+}  
     return 0;
-}
+} 
